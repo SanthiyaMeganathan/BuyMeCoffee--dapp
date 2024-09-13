b174d891d8e66400af9a0207bf3a345f2d0bc6b1
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-09-13 10:12:17 +0530

    buy me coffee

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-ignition");
+require("@nomicfoundation/hardhat-verify");
+require("@typechain/hardhat");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
--- ignition/modules/buy-coffee.js
+++ ignition/modules/buy-coffee.js
@@ -1,24 +1,12 @@
 const hre = require("hardhat");
 
 // Returns the Ether balance of a given address.
-// async function getBalance(address) {
-//   const balanceBigInt = await hre.ethers.provider.getBalance(address);
-//   return hre.ethers.utils.formatEther(balanceBigInt);
-// }
 async function getBalance(address) {
   const balanceBigInt = await hre.ethers.provider.getBalance(address);
-  return hre.ethers.utils.formateEther(balanceBigInt);
+  return hre.ethers.formatEther(balanceBigInt);
 }
 
 // Logs the Ether balances for a list of addresses.
-// async function printBalances(addresses) {
-//   let idx = 0;
-//   for (const address of addresses) {
-//     console.log(`Address ${idx} balance: `, await getBalance(address));
-//     idx++;
-//   }
-// }
-
 async function printBalances(addresses) {
   let idx = 0;
   for (const address of addresses) {
@@ -28,21 +16,8 @@ async function printBalances(addresses) {
 }
 
 // Logs the memos stored on-chain from coffee purchases.
-//this things come from our BuyMeACoffee contract
-// async function printMemos(memos) {
-//   for (const memo of memos) {
-//     const timestamp = memo.timestamp;
-//     const tipper = memo.name;
-//     const tipperAddress = memo.from;
-//     const message = memo.message;
-//     console.log(
-//       `At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`
-//     );
-//   }
-// }
-
 async function printMemos(memos) {
-  for (const memo in memos) {
+  for (const memo of memos) {
     const timestamp = memo.timestamp;
     const tipper = memo.name;
     const tipperAddress = memo.from;
@@ -55,27 +30,27 @@ async function printMemos(memos) {
 
 async function main() {
   // Get the example accounts we'll be working with.
-  // const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
   const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
 
   // We get the contract to deploy.
-  // const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
-  // const buyMeACoffee = await BuyMeACoffee.deploy();
-
-  const BuyMeACoffee = await hre.ethers.ContractFactory("BuyMeACoffee");
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
   const buyMeACoffee = await BuyMeACoffee.deploy();
-  // await buyMeACoffee.deployed();
+
   // Deploy the contract.
-  await buyMeACoffee.deployed();
+  // await buyMeACoffee.waitForDeployment();
   console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
 
   // Check balances before the coffee purchase.
   const addresses = [owner.address, tipper.address, buyMeACoffee.address];
+  // console.log("Owner address:", owner.address);
+  // console.log("Tipper address:", tipper.address);
+  // console.log("Tipper2 address:", tipper2.address);
+
   console.log("== start ==");
   await printBalances(addresses);
 
   // Buy the owner a few coffees.
-  const tip = { value: hre.ethers.utils.parseEther("1") };
+  const tip = { value: hre.ethers.parseEther("1") };
   await buyMeACoffee
     .connect(tipper)
     .buyCoffee("Carolina", "You're the best!", tip);
@@ -86,14 +61,14 @@ async function main() {
     .connect(tipper3)
     .buyCoffee("Kay", "I love my Proof of Knowledge", tip);
 
-  // Check balances after the coffee purchase.
+  //   // Check balances after the coffee purchase.
   console.log("== bought coffee ==");
   await printBalances(addresses);
 
-  // Withdraw.
+  //   // Withdraw.
   await buyMeACoffee.connect(owner).withdrawTips();
 
-  // Check balances after withdrawal.
+  //   // Check balances after withdrawal.
   console.log("== withdrawTips ==");
   await printBalances(addresses);
 
