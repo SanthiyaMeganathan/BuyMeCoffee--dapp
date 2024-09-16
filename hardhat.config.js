4ef78034b01fb22743a5973b6389e9520f364101
SanthiyaMeganathan <120907788+SanthiyaMeganathan@users.noreply.github.com>
2024-09-16 09:10:32 +0530

    hardhat-config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,20 +3,29 @@ require("@nomicfoundation/hardhat-ethers");
 require("@nomicfoundation/hardhat-ignition");
 require("@nomicfoundation/hardhat-verify");
 require("@typechain/hardhat");
+require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
+// const gasPrice = ethers.utils.parseUnits("2", "gwei"); // Set gas price to 2 gwei
+
+// const { vars } = require("hardhat/config");
+
 const SEPOLIA_URL = process.env.SEPOLIA_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-console.log(PRIVATE_KEY);
+const PRIVATE_KEY = process.env.PRIVATE_KEY.toString();
+const SEPOLIA_API_KEY = process.env.SEPOLIA_API_KEY;
+// const PRIVATE_KEY = vars.get(PRIVATE_KEY);
+console.log(PRIVATE_KEY.toString());
+console.log(SEPOLIA_URL);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
-  Networks: {
+  networks: {
     hardhat: {},
     sepolia: {
-      url: SEPOLIA_URL,
-      accounts: [`0${PRIVATE_KEY}`],
+      url: `https://eth-sepolia.g.alchemy.com/v2/fcUUXMMV-9rDjSWOJADoVEr5uKDs6fMC`,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
     },
   },
 };
